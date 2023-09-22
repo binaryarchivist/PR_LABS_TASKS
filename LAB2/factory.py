2d44ff803e1f1b37f6be1ff349b2bb145ffce247
Corneliu Nastas <cornel.nastas@corlab.tech>
2023-09-22 12:16:36 +0300

    lab2: finish hw

--- LAB2/factory.py
+++ LAB2/factory.py
@@ -2,6 +2,8 @@ from xml.etree import ElementTree
 from io import BytesIO
 
 from player import Player
+import player_pb2 as pb
+
 
 
 def dict_to_player(dict):
@@ -60,7 +62,7 @@ class PlayerFactory:
 
             for key, value in obj_dict.items():
                 child = ElementTree.SubElement(player, key)
-                if key is 'date_of_birth':
+                if key == 'date_of_birth':
                     child.text = str(value.strftime("%Y-%m-%d"))
                 else:
                     child.text = str(value)
@@ -76,10 +78,30 @@ class PlayerFactory:
         '''
             This function should transform a binary protobuf string into a list with Player objects.
         '''
-        pass
+        player_list = pb.PlayersList()
+        classes = pb.Class
+        player_list.ParseFromString(binary)
+        result = []
+
+        for player in player_list.player:
+            result.append(
+                Player(player.nickname, player.email, player.date_of_birth, player.xp, classes.Name(player.cls)))
+        return result
 
     def to_protobuf(self, list_of_players):
         '''
-            This function should transform a list with Player objects intoa binary protobuf string.
+            This function should transform a list with Player objects into a binary protobuf string.
         '''
-        pass
+        classes = pb.Class
+
+        serialized_players = pb.PlayersList()
+        for player in list_of_players:
+            serialized_player = serialized_players.player.add()
+
+            serialized_player.nickname = player.nickname
+            serialized_player.email = player.email
+            serialized_player.date_of_birth = player.date_of_birth.strftime("%Y-%m-%d")
+            serialized_player.xp = player.xp
+            serialized_player.cls = classes.Value(player.cls)
+
+        return serialized_players.SerializeToString()
